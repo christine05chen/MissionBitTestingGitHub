5603b3a03d0a0f28545a6202a577886775bb87d8
christine05chen <christine05chen@gmail.com>
2021-03-02 16:53:07 -0800

    hey

---
+++ lists.py
@@ -0,0 +1,20 @@
+# YOUR CODE HERE
+
+####################################################################################################
+# Take your list of restaurants from the previous challenge and print them out in a numbered list.
+# Example:
+# 1. <RESTAURANT 1>
+# 2. <RESTAURANT 2>
+# 3. <RESTAURANT 3>
+
+# YOUR CODE HERE
+
+####################################################################################################
+# Find out how far away each restaurant is (in miles) and store those distances in another list.
+# Then, print out the name of each restaurant AND how far away it is in a numbered list. Example:
+# 1. <RESTAURANT 1> is 2.4 miles away
+# 2. <RESTAURANT 2> is 6.8 miles away
+# 3. <RESTAURANT 3> is 10.2 miles away
+
+# YOUR CODE HERE
+
